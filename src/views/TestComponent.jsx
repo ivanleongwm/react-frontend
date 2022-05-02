53b5849ed0bb449a2359ef5ad27ef33287e8008c
ivanleongwm <ivanleongwm@gmail.com>
2022-05-02 17:54:13 +0800

    fetch to java backend added to test component, cors error encountered

--- src/views/TestComponent.jsx
+++ src/views/TestComponent.jsx
@@ -1,4 +1,32 @@
+import {BACKEND} from '../utils/utils';
+import { useEffect } from 'react';
+import urlcat from 'urlcat';
+
 export default function HelloWorld() {
+
+    const loginSuccessCheck = () => {
+        const url = urlcat(BACKEND, `/hello-world/path-variable/bean`);
+
+        fetch(url, {
+            method: "GET",
+            credentials: 'include',
+            headers: {
+            "Content-Type": "application/json",
+            },
+        })
+            .then((response) => {
+                console.log("RESPONSE OF TEST FETCH",response)
+                return response
+            })
+            .then((data) => {
+                console.log("DATA OF TEST FETCH",data)
+            })
+            .catch((error) => console.log(error));
+        };
+
+    useEffect(() => {
+        loginSuccessCheck()
+    },[])
 
     function handleSubmit() {
         console.log("retrieve clicked")
